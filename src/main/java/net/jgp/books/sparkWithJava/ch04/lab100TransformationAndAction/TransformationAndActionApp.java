29b03f6efb4eb756834b46a44259722df119941d
Jean Georges Perrin <jgp@jgp.net>
2018-06-13 07:57:27 -0400

    Added an example printing Hello world, to measure the speed of printing Hello world.

--- src/main/java/net/jgp/books/sparkWithJava/ch04/lab100TransformationAndAction/TransformationAndActionApp.java
+++ src/main/java/net/jgp/books/sparkWithJava/ch04/lab100TransformationAndAction/TransformationAndActionApp.java
@@ -1,4 +1,4 @@
-package net.jgp.books.sparkWithJava.ch04.lab100DuplicateColumns;
+package net.jgp.books.sparkWithJava.ch04.lab100TransformationAndAction;
 
 import static org.apache.spark.sql.functions.expr;
 
@@ -11,11 +11,7 @@ import org.apache.spark.sql.SparkSession;
  * 
  * @author jgp
  */
-public class DuplicateColumnApp {
-
-  enum Mode {
-    NOOP, CREATE_COLS, FULL
-  }
+public class TransformationAndActionApp {
 
   /**
    * main() is your entry point to the application.
@@ -23,71 +19,72 @@ public class DuplicateColumnApp {
    * @param args
    */
   public static void main(String[] args) {
-    DuplicateColumnApp app = new DuplicateColumnApp();
-    app.start(Mode.NOOP);
-    app.start(Mode.CREATE_COLS);
-    app.start(Mode.FULL);
+    TransformationAndActionApp app = new TransformationAndActionApp();
+    String mode = "noop";
+    if (args.length != 0) {
+      mode = args[0];
+    }
+    app.start(mode);
   }
 
   /**
    * The processing code.
    */
-  private void start(Mode mode) {
-
-    System.out.println("### Mode ... " + mode);
-
+  private void start(String mode) {
     long t0 = System.currentTimeMillis();
 
     // Step 1 - Creates a session on a local master
     SparkSession spark = SparkSession.builder()
         .appName("Analysing catalyst")
         .master("local")
-        .config("spark.driver.maxResultSize", "4g")
         .getOrCreate();
     long t1 = System.currentTimeMillis();
-    System.out.println("1. Creating a session ................. " + (t1 - t0));
+    System.out.println("1. Creating a session ........... " + (t1 - t0));
 
     // Step 2 - Reads a CSV file with header, stores it in a dataframe
     Dataset<Row> df = spark.read().format("csv")
         .option("header", "true")
         .load(
             "data/NCHS_-_Teen_Birth_Rates_for_Age_Group_15-19_in_the_United_States_by_County.csv");
+    Dataset<Row> df0 = df;
     long t2 = System.currentTimeMillis();
-    System.out.println("2. Loading initial dataset ............ " + (t2 - t1));
+    System.out.println("2. Loading initial dataset ...... " + (t2 - t1));
 
     // Step 3 - Build a bigger dataset
-    for (int i = 0; i < 7; i++) {
-      df = df.union(df);
+    for (int i = 0; i < 60; i++) {
+      df = df.union(df0);
     }
     long t3 = System.currentTimeMillis();
-    System.out.println("3. Building full dataset .............. " + (t3 - t2));
+    System.out.println("3. Building full dataset ........ " + (t3 - t2));
 
-    // Step 4- Cleanup. preparation
+    // Step 4 - Cleanup. preparation
     df = df.withColumnRenamed("Lower Confidence Limit", "lcl");
     df = df.withColumnRenamed("Upper Confidence Limit", "ucl");
     long t4 = System.currentTimeMillis();
-    System.out.println("4. Clean-up ........................... " + (t4 - t3));
+    System.out.println("4. Clean-up ..................... " + (t4 - t3));
 
-    if (mode != Mode.NOOP) {
+    // Step 5 - Transformation
+    if (mode.compareToIgnoreCase("noop") != 0) {
       df = df
           .withColumn("avg", expr("(lcl+ucl)/2"))
           .withColumn("lcl2", df.col("lcl"))
           .withColumn("ucl2", df.col("ucl"));
-      if (mode == Mode.FULL) {
+      if (mode.compareToIgnoreCase("full") == 0) {
         df = df
             .drop(df.col("avg"))
             .drop(df.col("lcl2"))
             .drop(df.col("ucl2"));
       }
     }
-
     long t5 = System.currentTimeMillis();
-    System.out.println("5. Transformations  ................... " + (t5 - t4));
+    System.out.println("5. Transformations  ............. " + (t5 - t4));
+
+    // Step 6 - Action
     df.collect();
     long t6 = System.currentTimeMillis();
-    System.out.println("6. Final action ....................... " + (t6 - t5));
+    System.out.println("6. Final action ................. " + (t6 - t5));
 
     System.out.println("");
-    System.out.println("# of records ....................... " + df.count());
+    System.out.println("# of records .................... " + df.count());
   }
 }
---
+++ src/main/java/net/jgp/books/sparkWithJava/ch04/lab900MiscStuff/HelloWorldApp.java
@@ -0,0 +1,24 @@
+package net.jgp.books.sparkWithJava.ch04.lab900MiscStuff;
+
+/**
+ * Hello world.
+ * 
+ * @author jgp
+ */
+public class HelloWorldApp {
+
+  /**
+   * main() is your entry point to the application, let's display 50,000 hello
+   * world.
+   * 
+   * @param args
+   */
+  public static void main(String[] args) {
+    long t0 = System.currentTimeMillis();
+    for (int i = 0; i < 50000; i++) {
+      System.out.println("Hello, world");
+    }
+    long t1 = System.currentTimeMillis();
+    System.out.println("1. Hello world ... " + (t1 - t0));
+  }
+}
