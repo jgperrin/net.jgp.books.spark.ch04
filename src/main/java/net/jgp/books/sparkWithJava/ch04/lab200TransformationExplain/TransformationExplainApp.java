33f982bc8a262c401cf2a325266a6a337e03a005
Jean Georges Perrin <jgp@jgp.net>
2018-06-28 06:14:12 -0400

    Explain example simplified

--- src/main/java/net/jgp/books/sparkWithJava/ch04/lab200TransformationExplain/TransformationExplainApp.java
+++ src/main/java/net/jgp/books/sparkWithJava/ch04/lab200TransformationExplain/TransformationExplainApp.java
@@ -13,60 +13,47 @@ import org.apache.spark.sql.SparkSession;
  */
 public class TransformationExplainApp {
 
-  /**
-   * main() is your entry point to the application.
-   * 
-   * @param args
-   */
-  public static void main(String[] args) {
-    TransformationExplainApp app = new TransformationExplainApp();
-    String mode = "noop";
-    if (args.length != 0) {
-      mode = args[0];
-    }
-    app.start(mode);
-  }
-
-  /**
-   * The processing code.
-   */
-  private void start(String mode) {
-
-    // Step 1 - Creates a session on a local master
-    SparkSession spark = SparkSession.builder()
-        .appName("Analysing Catalyst's behavior")
-        .master("local")
-        .getOrCreate();
-
-    // Step 2 - Reads a CSV file with header, stores it in a dataframe
-    Dataset<Row> df = spark.read().format("csv")
-        .option("header", "true")
-        .load(
-            "data/NCHS_-_Teen_Birth_Rates_for_Age_Group_15-19_in_the_United_States_by_County.csv");
-    Dataset<Row> df0 = df;
-
-    // Step 3 - Build a bigger dataset
-    for (int i = 0; i < 60; i++) {
-      df = df.union(df0);
+    /**
+     * main() is your entry point to the application.
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        TransformationExplainApp app = new TransformationExplainApp();
+        String mode = "noop";
+        if (args.length != 0) {
+            mode = args[0];
+        }
+        app.start(mode);
     }
 
-    // Step 4 - Cleanup. preparation
-    df = df.withColumnRenamed("Lower Confidence Limit", "lcl");
-    df = df.withColumnRenamed("Upper Confidence Limit", "ucl");
+    /**
+     * The processing code.
+     */
+    private void start(String mode) {
 
-    // Step 5 - Transformation
-    df = df
-        .withColumn("avg", expr("(lcl+ucl)/2"))
-        .withColumn("lcl2", df.col("lcl"))
-        .withColumn("ucl2", df.col("Upper Confidence Limit"));
+        // Step 1 - Creates a session on a local master
+        SparkSession spark = SparkSession.builder().appName("Analysing Catalyst's behavior").master("local")
+                .getOrCreate();
 
-    df = df
-        .drop(df.col("avg"))
-        .drop(df.col("lcl2"))
-        .drop(df.col("ucl2"));
+        // Step 2 - Reads a CSV file with header, stores it in a dataframe
+        Dataset<Row> df = spark.read().format("csv").option("header", "true")
+                .load("data/NCHS_-_Teen_Birth_Rates_for_Age_Group_15-19_in_the_United_States_by_County.csv");
+        Dataset<Row> df0 = df;
 
-    // Step 6 - Action
-    df.explain();
+        // Step 3 - Build a bigger dataset
+        df = df.union(df0);
 
-  }
+        // Step 4 - Cleanup. preparation
+        df = df.withColumnRenamed("Lower Confidence Limit", "lcl");
+        df = df.withColumnRenamed("Upper Confidence Limit", "ucl");
+
+        // Step 5 - Transformation
+        df = df.withColumn("avg", expr("(lcl+ucl)/2")).withColumn("lcl2", df.col("lcl")).withColumn("ucl2",
+                df.col("ucl"));
+
+        // Step 6 - Action
+        df.explain();
+
+    }
 }
