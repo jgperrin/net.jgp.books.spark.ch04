955c4d5b478204e6be9f04c2fb1a046003623486
Jean-Georges Perrin <jgp@jgp.net>
2019-08-25 09:47:11 -0400

    Update

--- src/main/java/net/jgp/books/spark/ch04/lab900_misc/HelloWorldApp.java
+++ src/main/java/net/jgp/books/spark/ch04/lab900_misc/HelloWorldApp.java
@@ -8,9 +8,9 @@ package net.jgp.books.spark.ch04.lab900_misc;
 public class HelloWorldApp {
 
   /**
-   * main() is your entry point to the application, let's display 50,000 hello
-   * world: it takes about the same time as creating 7.5m datapoints using
-   * Spark, just for the fun of it...
+   * main() is your entry point to the application, let's display 50,000
+   * hello world: it takes about the same time as creating 7.5m datapoints
+   * using Spark, just for the fun of it...
    * 
    * @param args
    */
